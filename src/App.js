d0a4f8f06e48ddea247de0b7c371f90a36ddddcd
Zsolt Nagy <zsolt.nagy@jaystack.com>
2022-06-24 13:08:56 +0200

    chore: working prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Graph from "react-vis-network-graph";
 
+import { org } from "./constant/org";
 import "./index.css";
+
+
+
 
 const App = (props) => {
 
+  const generateEdges = () => {
+    return org.map((connection) => {
+      return connection.parentPersons.map(parent => ({from: parent.id, to: connection.id}))
+    }).flat();
+  }
+
+
   const graph = {
-    nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tootip text" },
-      { id: 2, label: "Node 2", title: "node 2 tootip text" },
-      { id: 3, label: "Node 3", title: "node 3 tootip text" },
-      { id: 4, label: "Node 4", title: "node 4 tootip text" },
-      { id: 5, label: "Node 5", title: "node 5 tootip text" }
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      {from: 1, to: 4},
-      { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 }
-    ]
+    nodes: org,
+    edges: generateEdges(),
   };
 
   const options = {
@@ -45,7 +44,9 @@ const App = (props) => {
         graph={graph}
         options={options}
         events={events}
+      
         getNetwork={network => {
+          console.log(network);
           //  if you want access to vis.js network api you can set the state in a parent component using this property
         }}
       />
---
+++ src/constant/org.js
@@ -0,0 +1,82 @@
+const relationShipTypes = {
+    HARD_SKILLS: "HARD_SKILLS",
+    SOFT_SKILLS: "SOFT_SKILLS",
+    OPERATIONAL: "OPERATIONAL",
+    PROJECT: "PROJECT",
+    MENTORSHIP: "MENTORSHIP"
+};
+
+export const org = [
+    {
+        "id": 1,
+        "label": "Nick",
+        "role": "CEO",
+        "karmaScore": 2022,
+        "parentPersons": [],
+        "softSkills": [],
+        "hardSkills": []
+    },
+    {
+        "id": 2,
+        "label": "Z",
+        "karmaScore": 1400,
+        "role": "VP of Engineering",
+        "parentPersons": [{ "id": 1, "type": relationShipTypes.OPERATIONAL }],
+        "softSkills": [],
+        "hardSkills": []
+    },
+    {
+        "id": 3,
+        "label": "Javier",
+        "karmaScore": 800,
+        "role": "Team Lead",
+        "parentPersons": [{ "id": 2, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": []
+    },
+    {
+        "id": 4,
+        "label": "Natalie",
+        "karmaScore": 800,
+        "role": "Designer",
+        "parentPersons": [{ "id": 3, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": ["design", "css"]
+    },
+    {
+        "id": 5,
+        "label": "Zsolt",
+        "karmaScore": 0,
+        "role": "Backend Lead",
+        "parentPersons": [{ "id": 3, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": ["node", "react"]
+    },
+    {
+        "id": 6,
+        "label": "Viktor",
+        "karmaScore": 0,
+        "role": "Frontend Engineer",
+        "parentPersons": [{ "id": 4, "type": relationShipTypes.PROJECT }, { "id": 3, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": ["react", "css", "html"]
+    },
+    {
+        "id": 7,
+        "label": "Elizabeth",
+        "karmaScore": 0,
+        "role": "Frontend Engineer",
+        "parentPersons": [{ "id": 4, "type": relationShipTypes.PROJECT }, { "id": 3, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": ["react", "css", "html"]
+    },
+    {
+        "id": 8,
+        "label": "Dávid",
+        "karmaScore": 0,
+        "role": "Backend Engineer",
+        "parentPersons": [{ "id": 5, "type": relationShipTypes.PROJECT }, { "id": 3, "type": relationShipTypes.PROJECT }],
+        "softSkills": [],
+        "hardSkills": ["react", "css", "html"]
+    }
+];
